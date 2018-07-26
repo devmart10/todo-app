c26dfebe3020dfb339b5c66ab196eac443cb1281
devmart10 <devmart10@gmail.com>
2018-07-25 17:35:02 -0700

    fix 3

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App.js"
 
-import 'bootstrap/dist/css/bootstrap.css'
-
 ReactDOM.render(
     <App />, 
     document.getElementById("root")
